ac7a42071d5e48bc17840bff7cc00f0c9688ff92
rodrigobnm <rbnm@cesar.school>
2024-06-14 16:38:08 -0300

    .

--- 04.py
+++ 04.py
@@ -5,20 +5,20 @@
 # lista e ao digitar 'FIM' passe para a segunda lista
 # ao digitar 'FIM' novamente a função seja executada.
 
-l1 = []
+l1 = set()
 x = 0
 y = 0
-l2 = []
+l2 = set()
 
 while x != "FIM":
    x = input("digite valores da lista 1 ou FIM: ")
    if x != "FIM":
-       l1.append(int(x))
+       l1.add(int(x))
 
 while y != "FIM":
    y = input("digite valores da lista 2 ou FIM: ")
    if y != "FIM":
-       l2.append(int(y))
+       l2.add(int(y))
 
 #transformando as listas em sets e printando a intersecção
-print(set(l1) & set(l2))
+print(l1 & l2)
--- x.py
+++
@@ -1,6 +0,0 @@
-def x(r):
-    return 3-1, 4 + 2
-
-x, y = x(2)
-print(x)
-print(y)

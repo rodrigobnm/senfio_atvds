3cc9c9ce8acadd735f8439863fa0ebbd754379ac
rodrigobnm <rbnm@cesar.school>
2024-07-01 15:38:10 -0300

    finalização completa sprint 3 - atividades

--- sprint_3/sprint_3_atvds/04/04.py
+++ sprint_3/sprint_3_atvds/04/04.py
@@ -1,8 +1,6 @@
 # ->> Ex 04) Crie um app que recebe um número inteiro e cria uma matriz, aleatória, através da lib 'numpy' e exiba o resultado.
 #    * Utilize ambiente virtual
 #    * Guarde os dados do ambiente virtual num arquivo 'requirements.txt'
-
-
 import numpy
 
 tamanhoMatriz = int(input("Tamanho da matriz: "))

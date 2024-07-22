cb68ad12ed932950c506c65a9a80fbd321090def
rodrigobnm <rbnm@cesar.school>
2024-07-22 15:27:30 -0300

    inicio da atividade do sprint 4

---
+++ sprint_4_poo/atividade_sprint_4/main.py
@@ -0,0 +1,26 @@
+class Book():
+    def __init__(self, title, author):
+        self.__title = title
+        self.__author = author
+        self.__available = True
+    def loan(self):
+        if self.__available is True:
+            self.__available = False
+            return print(f"Livro, {self.__title}, Emprestado Com Sucesso! ")
+        else: 
+            return print(f"Livro, {self.__title}, Nao Disponivel")
+    def give_back(self):
+        self.__available = True
+        return print(f"{self.__title}, Marcado Como Disponivel! ")
+    def its_available(self):
+        if self.__available is True:
+            return True
+        else:
+            return False
+    def __str__(self):
+        print()
+class Client():
+    def __init__(self, name):
+        self.__name = name
+        self.__borrowed_bocks = __borrowed_bocks
+    def lend_book()
